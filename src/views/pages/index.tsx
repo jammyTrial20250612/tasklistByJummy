5618860e3c5f4d8c6bf2a8495ab6e15a65b7f4fa
jam <jummy.40405202.jummy@gmail.com>
2025-12-14 00:03:45 +0900

    delete some func

--- src/views/pages/index.tsx
+++ src/views/pages/index.tsx
@@ -7,25 +7,8 @@ import Slack from "../components/unique/Slack/Slack";
 import Servermanagement from "../components/unique/Servermanagement/Servermanagement";
 import Tips from "../components/unique/Tips/Tips";
 import Coding from "../components/unique/Coding/Coding";
-import { CRUD } from "../components/crud/CRUD";
-import { MockRead } from "../components/crud/MockRead";
-import { items, item } from "../../types";
-// import { ListItemSecondaryAction } from "@material-ui/core";
-import { AxiosGet } from "../components/crud/AxiosGet";
-import UserList from "../components/crud/UserList";
-import { MockPython } from "../components/crud/MockPython";
 
 const Pages: React.FC = () => {
-  const items1 = [
-    {
-      id: 1,
-      param1: 100,
-      param2: 200,
-      param3: 300,
-      name: "phoo",
-    },
-  ];
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -35,10 +18,6 @@ const Pages: React.FC = () => {
       <Route path="/slack" element={<Slack />} />
       <Route path="/servermanagement" element={<Servermanagement />} />
       <Route path="/tips" element={<Tips />} />
-      <Route path="/crud" element={<CRUD />} />
-      <Route path="/axios_get" element={<AxiosGet />} />
-      <Route path="/userlist" element={<UserList />} />
-      <Route path="/mockread" element={<MockRead />} />
     </Routes>
   );
 };
